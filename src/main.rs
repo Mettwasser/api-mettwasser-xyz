0808a2f4ed79d728b687f5db021aecb7d736433c
Kai Konze <kaicrafter2000@gmail.com>
2023-12-28 17:12:18 +0100

    fix: port for docker should be working now

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ async fn main() {
     };
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
