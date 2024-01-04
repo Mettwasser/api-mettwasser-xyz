92be1e5eeaeb7c74663b1691e90a69ba9f5bd75c
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-04 09:47:05 +0100

    feat: add background animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./templates/*.html", "./assets/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                fadein: {
+                    "0%": { opacity: 0, backgroundColor: "#000000" },
+                    "100%": { opacity: 100, backgroundColor: "#262626" },
+                },
+            },
+            animation: {
+                fadein: "fadein 2s forwards",
+            },
+        },
     },
     plugins: [],
 };
