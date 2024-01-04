52627d4bb57bd646a64c74bbbadff22246df5604
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-04 09:21:22 +0100

    chore: separate assets to assets and build and add new assets

--- src/assets.rs
+++ src/assets.rs
@@ -5,6 +5,7 @@ use axum::{
 };
 
 use crate::ASSETS;
+use crate::BUILD;
 
 pub async fn assets(Path(path): Path<String>) -> impl IntoResponse {
     let mut headers = HeaderMap::new();
@@ -14,12 +15,34 @@ pub async fn assets(Path(path): Path<String>) -> impl IntoResponse {
         (
             StatusCode::OK,
             headers,
-            ASSETS
+            BUILD
                 .get_file("index.css")
                 .unwrap()
                 .contents_utf8()
                 .unwrap(),
         )
+    } else if path == "index.js" {
+        headers.insert(header::CONTENT_TYPE, "application/javascript".parse().unwrap());
+        (
+            StatusCode::OK,
+            headers,
+            ASSETS
+                .get_file("scripts/index.js")
+                .unwrap()
+                .contents_utf8()
+                .unwrap(),
+        ) 
+    } else if path == "typewriter.js" {
+        headers.insert(header::CONTENT_TYPE, "application/javascript".parse().unwrap());
+        (
+            StatusCode::OK,
+            headers,
+            ASSETS
+                .get_file("scripts/typewriter.js")
+                .unwrap()
+                .contents_utf8()
+                .unwrap(),
+        )  
     } else {
         (StatusCode::NOT_FOUND, headers, "")
     }
