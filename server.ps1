6b10d34f59e2d75657e422ac6b271efa9b0bd112
Mettwasser <kaicrafter2000@gmail.com>
2024-01-06 14:30:02 +0100

    feat: Scripts for Starting & Stopping autobuild with cargo & tailwindcss

--- server.ps1
+++
@@ -1,6 +0,0 @@
-# Starts server with cargo and tailwind
-# Cargo and tailwind cli are required to run this ps1 script successfully
-
-Write-Host "Dev server started at http://127.0.0.1:3000" -ForegroundColor yellow
-cargo watch -w . -x run &
-tailwindcss -c ./tailwind.config.js -i ./assets/styles/index.css -o ./build/index.css --watch &
