da1ba4a9026ef81541c3f8211a195f3866b31c4f
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-06 11:08:28 +0100

    chore: tailwind cli should now build successfully

--- server.ps1
+++ server.ps1
@@ -3,4 +3,4 @@
 
 Write-Host "Dev server started at http://127.0.0.1:3000" -ForegroundColor yellow
 cargo watch -w . -x run &
-tailwindcss -c ./tailwind.config.js -i ./assets/styles/index.css -o ./build/index.css --watch &
+tailwindcss -c ./tailwind.config.js -i ./assets/styles/index.css -o ./build/index.css --watch
