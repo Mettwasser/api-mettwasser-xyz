38c6d297f35fc987354780d9c4b5319f6e714b8b
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-29 18:34:32 +0100

    feat: better animations

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,9 +5,14 @@ import typewriter from "./typewriter.js";
 typewriter("#main-heading", {
     text: "api.mettwasser.xyz",
     speed: 100,
-    onFinish: () => typewriter("#sub-heading", {
-        text: "Transparent and community-driven.",
-        speed: 65,
-    }),
+    onFinish: () =>
+        typewriter("#sub-heading", {
+            text: "Transparent and community-driven.",
+            speed: 30,
+            onFinish: () => {
+                document
+                    .querySelector("#socials")
+                    .classList.add("animate-fadein");
+            },
+        }),
 });
-
